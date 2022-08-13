9f2b0b9ec3982a59fad6d4234cda80399f6cb41e
Amr <asolyma@hotmail.com>
2022-08-13 19:30:26 +0200

    created  /post route to get all posts

---
+++ pages/api/post/index.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { validateToken } from "../../../lib/Auth";
+import { prisma } from "../../../prisma/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        auhtor: true,
+      },
+      orderBy: {
+        createdat: "desc",
+      },
+    });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
