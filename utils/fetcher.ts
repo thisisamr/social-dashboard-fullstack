94c88ff838c065ccde748810dbddd67bcad77d07
Asolyma <asolyma@hotmail.com>
2022-08-21 09:23:25 +0200

    fetcher update
    updated fetcher to throw 🚀 ERROR  for not 2xx responses

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,10 +1,22 @@
-export default function fetcher<T, U>(url: T, data?: U) {
-  return fetch(`${window.location.origin}/api/${url}`, {
+export default async function fetcher<T, U>(url: T, data?: U) {
+  const res = await fetch(`${window.location.origin}/api/${url}`, {
     method: data ? "POST" : "GET",
     credentials: "include",
     headers: {
       "Content-type": "Application/json",
     },
     body: JSON.stringify(data),
-  }).then((d) => d.json());
+  });
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+
+  if (!res.ok) {
+    const error: any = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
 }
