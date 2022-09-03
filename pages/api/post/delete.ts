2650154e79641f728f6daca3987be6c847faa173
Asolyma <asolyma@hotmail.com>
2022-09-03 10:19:27 +0200

    fixed delete route

--- pages/api/post/delete.ts
+++ pages/api/post/delete.ts
@@ -8,37 +8,30 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const token = req.cookies.T_ACCESS_TOKEN || req.headers.authorization;
-  if (!token) {
-    return res.status(401).json({ error: "no auth" });
-  }
-  const user = await validateToken(token);
-  if (req.method !== "POST") {
-    return res.status(404).json({ error: "unsupported http verb" });
-  }
   const { id } = req.body;
   if (!id || isNaN(id)) {
     return res.status(400).json({ error: "bad user Input" });
   }
-  //go to the database
+  if (req.method !== "POST") {
+    return res.status(404).json({ error: "unsupported http verb" });
+  }
+  if (!token) {
+    return res.status(401).json({ error: "no auth" });
+  }
   try {
-    //check if the post.author.id = user.id
-    const found = await prisma.post.findUnique({
+    const user = await validateToken(token);
+    const deleted = await prisma.post.deleteMany({
       where: {
         id,
+        AND: {
+          authorid: user?.id,
+        },
       },
     });
-    if (found?.authorid === user.id) {
-      const deleted = await prisma.post.delete({
-        where: {
-          id,
-        },
-      });
-      return res.status(200).json({ id: deleted.id });
-    } else {
-      return res
-        .status(401)
-        .json({ error: "not authorized to delete this post" });
+    if (deleted.count == 0) {
+      return res.status(400).json({ error: "bad Request" });
     }
+    return res.status(200).json({ id: id });
   } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
